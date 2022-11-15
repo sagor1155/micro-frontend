266a1d6533b557029fcb95062491e253636275b2
sagor <sagor.eee.cuet@gmail.com>
2022-11-16 01:02:24 +0600

    update angular, add & config webpack module federation, remote module loading works

--- micro-frontend-demo/projects/products/webpack.config.js
+++ micro-frontend-demo/projects/products/webpack.config.js
@@ -1,49 +1,15 @@
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const mf = require("@angular-architects/module-federation/webpack");
-const path = require("path");
-const share = mf.share;
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
-const sharedMappings = new mf.SharedMappings();
-sharedMappings.register(
-  path.join(__dirname, '../../tsconfig.json'),
-  [/* mapped paths to share */]);
+module.exports = withModuleFederationPlugin({
 
-module.exports = {
-  output: {
-    uniqueName: "products",
-    publicPath: "auto"
+  name: 'products',
+
+  exposes: {
+    './DashboardModule': './projects/products/src/app/dashboard/dashboard.module.ts',
   },
-  optimization: {
-    runtimeChunk: false
-  },   
-  resolve: {
-    alias: {
-      ...sharedMappings.getAliases(),
-    }
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
   },
-  experiments: {
-    outputModule: true
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-        library: { type: "module" },
 
-        name: "products",
-        filename: "remoteEntry.js",
-        exposes: {
-            './DashboardModule': './projects/products/src/app/dashboard/dashboard.module.ts',
-        },        
-
-        shared: share({
-          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-
-          ...sharedMappings.getDescriptors()
-        })
-        
-    }),
-    sharedMappings.getPlugin()
-  ],
-};
+});
